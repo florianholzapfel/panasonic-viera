cc9a9ddbc46a0cac0ad2b210366d636cd0b0907a
Charles-HL <61110764+Charles-HL@users.noreply.github.com>
2026-09-11 11:04:24 +0200

    Add PAC input and picture mode controls

--- panasonic_viera/__init__.py
+++ panasonic_viera/__init__.py
@@ -1,45 +1,49 @@
 """Module to interact with your Panasonic Viera TV."""
 
 # Import all classes and constants for external use
+from .apps import Apps
 from .constants import (
-    URN_RENDERING_CONTROL,
-    URN_REMOTE_CONTROL,
-    URL_TEMPLATE,
-    URL_CONTROL_NRC_DDD,
-    URL_CONTROL_NRC_DEF,
+    BLOCK_SIZE,
+    DEFAULT_PORT,
+    TV_TYPE_ENCRYPTED,
+    TV_TYPE_NONENCRYPTED,
     URL_CONTROL_DMR,
     URL_CONTROL_NRC,
-    TV_TYPE_NONENCRYPTED,
-    TV_TYPE_ENCRYPTED,
-    DEFAULT_PORT,
-    BLOCK_SIZE,
+    URL_CONTROL_NRC_DDD,
+    URL_CONTROL_NRC_DEF,
+    URL_CONTROL_PAC,
+    URL_TEMPLATE,
+    URN_PRO_AV_CONTROL,
+    URN_REMOTE_CONTROL,
+    URN_RENDERING_CONTROL,
     pad,
 )
+from .exceptions import EncryptionRequired, SOAPError
 from .keys import Keys
-from .apps import Apps
-from .exceptions import SOAPError, EncryptionRequired
 from .remote_control import RemoteControl
 
 # Export all classes and constants for public API
 __all__ = [
-    # Classes
-    "Keys",
-    "Apps",
-    "SOAPError",
-    "EncryptionRequired", 
-    "RemoteControl",
-    # Constants
-    "URN_RENDERING_CONTROL",
-    "URN_REMOTE_CONTROL",
-    "URL_TEMPLATE",
-    "URL_CONTROL_NRC_DDD",
-    "URL_CONTROL_NRC_DEF",
+    "BLOCK_SIZE",
+    "DEFAULT_PORT",
+    "TV_TYPE_ENCRYPTED",
+    "TV_TYPE_NONENCRYPTED",
     "URL_CONTROL_DMR",
     "URL_CONTROL_NRC",
-    "TV_TYPE_NONENCRYPTED",
-    "TV_TYPE_ENCRYPTED",
-    "DEFAULT_PORT",
-    "BLOCK_SIZE",
+    "URL_CONTROL_NRC_DDD",
+    "URL_CONTROL_NRC_DEF",
+    "URL_CONTROL_PAC",
+    "URL_TEMPLATE",
+    "URN_PRO_AV_CONTROL",
+    "URN_REMOTE_CONTROL",
+    # Constants
+    "URN_RENDERING_CONTROL",
+    "Apps",
+    "EncryptionRequired",
+    # Classes
+    "Keys",
+    "RemoteControl",
+    "SOAPError",
     # Utility functions
     "pad",
 ]
